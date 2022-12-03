741e862d5bae1b3117671544a9563e3918a13503
lucas-309 <73149140+lucas-309@users.noreply.github.com>
2022-12-02 21:06:25 -0800

    Update Advent Day 2.py

--- Advent Day 2.py
+++ Advent Day 2.py
@@ -23,3 +23,27 @@ for x in f:
         score += 6
 
 print(score)
+
+# Part 2
+score = 4
+f = open("abc.txt", "r")
+for x in f:
+    if x == "A X\n":
+        score += 3
+    if x == "A Y\n":
+        score += 4
+    if x == "A Z\n":
+        score += 8
+    if x == "B X\n":
+        score += 1
+    if x == "B Y\n":
+        score += 5
+    if x == "B Z\n":
+        score += 9
+    if x == "C X\n":
+        score += 2
+    if x == "C Y\n":
+        score += 6
+    if x == "C Z\n":
+        score += 7
+print(score)
