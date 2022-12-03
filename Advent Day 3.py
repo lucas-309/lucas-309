ae7a69525ea21a95a735b16380bd531df7a33bb8
lucas-309 <73149140+lucas-309@users.noreply.github.com>
2022-12-02 22:14:30 -0800

    Update Advent Day 3.py

--- Advent Day 3.py
+++ Advent Day 3.py
@@ -21,3 +21,39 @@ for x in f:
         
 
 print(sum)
+
+# Part 2
+sum = 0
+letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+with open("3.txt") as fp:
+    cnt = 1
+    for i in range(1,301):
+        if i % 3 == 1:
+            line = fp.readline()
+            temp1 = line.strip()
+            
+            cnt += 1 
+            line = fp.readline()
+            temp2 = line.strip()
+            
+            cnt += 1 
+            line = fp.readline()
+            temp3 = line.strip()
+            for a in range(len(temp1)):
+                for b in range(len(temp2)):
+                    if temp1[a] == temp2[b]:
+                        for c in range(len(temp3)):
+                            if temp1[a] == temp3[c]:
+                                sum += letters.index(temp1[a]) + 1
+                                break
+                        else:
+                            continue
+                        break
+                else:
+                    continue
+                break
+       
+
+
+
+print(sum)
